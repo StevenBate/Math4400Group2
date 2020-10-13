21de10e5d0b32fb8a2f4f7c5b6da85384f6e5301
StevenBate <39107347+StevenBate@users.noreply.github.com>
2020-10-12 22:16:22 -0600

    Scenario 2 and 4

--- groupProject.R
+++ groupProject.R
@@ -1,7 +1,10 @@
 #Start of file
 library('class')
+library(pROC)
+library('MASS')
 
-data.generate = function(mu1, mu2, s, n , p){
+#Generate data
+data.generate = function(mu1, mu2, s, n, p){
   
   y = as.factor(c(rep(1,n),rep(2,n)))
   M = matrix (NA,  nrow = 2*n, ncol = p, byrow = 1) 
@@ -14,15 +17,99 @@ data.generate = function(mu1, mu2, s, n , p){
   df = data.frame(df)
   return(df)
 }
-n = 500
-df = data.generate(1,-1, 1, n, 2)
+
+#Split into train / test data
+train_Test_Split = function(df, seed)
+{
+  set.seed(seed)
+  test.index = sample(c(1:dim(df)[1]), floor(dim(df)[1]/2), replace = FALSE)
+  
+  assign("df.train", df[-test.index,], envir = .GlobalEnv)
+  assign("df.test", df[test.index,], envir = .GlobalEnv)
+  
+  assign("train.X", df.train[,-1], envir = .GlobalEnv)
+  assign("train.Y", df.train[,1], envir = .GlobalEnv)
+  assign("test.X", df.test[,-1], envir = .GlobalEnv)
+  assign("test.Y", df.test[,1], envir = .GlobalEnv)
+  
+  return(test.index)
+}
+
+#Qda algorythm
+generate.qda = function(df.train, df.test){
+  qda.fit = qda(y ~ . - y, data = df.train)
+  
+  qda.pred = predict(qda.fit,df.test[,-1])
+  qda.accuracy = mean(qda.pred$class==df.test[,1])
+  
+  return(qda.accuracy)
+}
+
+#Lda algorythm
+generate.lda = function(df.train, df.test){
+  lda.fit = lda(y ~ . - y, data = df.train)
+  
+  lda.pred = predict(lda.fit,df.test[,-1])
+  lda.accuracy = mean(lda.pred$class==df.test[,1])
+  
+  return(lda.accuracy)
+}
+
+?lda
+?qda
 head(df)
 
-#Sample and break into training and testing data
-test.index = sample(c(1:dim(df)[1]), floor(n/2),replace = TRUE)
-df.test = df[test.index,]
-df.train = df[-test.index,]
-train.X = df.train[,c("V2", "V3")]
-test.X = df.test[,c("V2", "V3")]
-train.Y = df.train[,c("y")]
-test.Y = df.test[,c("y")]
+
+scenario.two = function(){
+  n = 500
+  df = data.generate(1,-1, 1, n, 2)
+  
+  for (col in colnames(df)){
+    print(col)
+    if (col != "y")
+    {
+      df[,col] = scale(df[,col])
+    }
+  }
+  
+  test.index = train_Test_Split(df, 1)
+  qda.accuracy = generate.qda(df.train, df.test)
+  lda.accuracy = generate.lda(df.train, df.test)
+  
+  assign("qda.accuracy.two", qda.accuracy, envir = .GlobalEnv)
+  assign("lda.accuracy.two", lda.accuracy, envir = .GlobalEnv)
+}
+
+scenario.four = function(){
+  n = 500
+  df = data.generate(1,-1, 1, n, 20)
+  
+  for (col in colnames(df)){
+    print(col)
+    if (col != "y")
+    {
+      df[,col] = scale(df[,col])
+    }
+  }
+  
+  test.index = train_Test_Split(df, 1)
+  qda.accuracy = generate.qda(df.train, df.test)
+  lda.accuracy = generate.lda(df.train, df.test)
+  
+  assign("qda.accuracy.four", qda.accuracy, envir = .GlobalEnv)
+  assign("lda.accuracy.four", lda.accuracy, envir = .GlobalEnv)
+}
+
+scenario.two()
+scenario.four()
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
